d958fa819c207a664aca11923b73a3457444e489
cristobalhenriquez <cristobalhb@live.com>
2022-07-09 21:12:46 -0300

    header finalizado con javascript

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+var boton=document.querySelector(".header__icono")
+var menu=document.querySelector(".header__navigation")
+var cont=0
+
+boton.onclick=function (){
+  if(cont%2==0){
+  menu.classlist.add("header__navigation--active")
+  menu.classlist.remove("header__navigation--disabled")
+ }
+
+ else{
+ menu.classlist.remove("header__navigation--active")
+ menu.classlist.add("header__navigation--disabled")
+ }
+
+  cont++
+}
